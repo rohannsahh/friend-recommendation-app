9c38af4b15bba7cae4a8e7d5ddaf8ec684fbae86
Rohan <rohannsahh@gmail.com>
2025-01-05 23:11:56 +0530

    deploy fix

--- frontend/src/components/sign-up.tsx
+++ frontend/src/components/sign-up.tsx
@@ -55,7 +55,7 @@ export function SignUpComponent() {
     }
 
   }
-  console.log(API_BASE_URL);
+  console.log(import.meta.env.VITE_API_BASE_URL);
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
